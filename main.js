718ba5d3bda7c811a2ffffdcf41251871e619a93
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-16 19:22:18 +0100

    fine esercizio

---
+++ main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emailArray:[]
+      }
+    },
+    created(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=>{
+                this.emailArray.push(response.data.response)
+            })   
+        }
+    }
+  }).mount('#app')
